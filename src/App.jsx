5e0098afd52e08ce67d3abe19b0f8fe01f15a74a
John Dykes <98johndykes@gmail.com>
2025-04-14 06:45:04 -0400

    useState, useRef, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useRef, useEffect } from 'react';
+
 function Todo({ children, ...props }) {
   return (
     <div>
@@ -7,27 +9,49 @@ function Todo({ children, ...props }) {
   );
 }
 
+const initialTodos = [
+  { id: 1, task: 'Wash the dishes', isComplete: false },
+  { id: 2, task: 'Do your homework', isComplete: false },
+  { id: 3, task: 'Play videogames', isComplete: true },
+];
+
 function App() {
-  const todos = [
-    { id: 1, task: 'Wash the dishes', isComplete: false },
-    { id: 3, task: 'Do your homework', isComplete: false },
-    { id: 4, task: 'Play videogames', isComplete: true },
-    { id: 5, task: 'Solve world hunger', isComplete: false },
-    { id: 8, task: 'Solve my hunger', isComplete: true },
-  ];
+  const [todos, setTodos] = useState(initialTodos);
+  const ref = useRef();
+  useEffect(() => {
+    async function fetchInitialTodos() {
+      const response = await fetch(
+        'https://todos-api.98johndykes.workers.dev/'
+      );
+      setTodos(await response.json());
+    }
+
+    fetchInitialTodos();
+  }, []);
+
+  function handleNewTodo() {
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        task: ref.current.value,
+        isComplete: false,
+      },
+    ]);
+  }
 
   return (
     <>
       <h1>Todo List</h1>
 
       <div className="new-todo-input">
-        <input type="text" placeholder="Add new todo item" />
-        <button>Add Todo</button>
+        <input type="text" placeholder="Add new todo item" ref={ref} />
+        <button onClick={handleNewTodo}>Add Todo</button>
       </div>
 
       <div className="todo-container">
         {todos.map((todo) => (
-          <Todo>{todo.task}</Todo>
+          <Todo key={todo.id}>{todo.task}</Todo>
         ))}
       </div>
     </>
